82fc647dc289f409150caa3b8f09b60faea54baf
ym1018 <73922043+ym1018@users.noreply.github.com>
2023-03-28 13:21:01 +0900

    메인 화면 레이아웃 (1차)

---
+++ KWU-facilities/js/main-map.js
@@ -0,0 +1,13 @@
+let hover_container = document.querySelector('.hover-container');
+// let layer = document.querySelectorAll('.hover-layer');
+
+// 마우스에 따라 움직이는 이미지
+// container.onmousemove = function(e) {
+//     let X = e.pageX;
+//     let Y = e.pageY;
+  
+//     layer[0].style.transform = 'translate(' + X/100 + 'px, ' + Y/100 + 'px)';
+//     layer[1].style.transform = 'translate(' + X/100*-2 + 'px, ' + Y/100*-2 + 'px)';
+//     layer[2].style.transform = 'translate(' + X/100*-4 + 'px, ' + Y/100*-4 + 'px)';
+//     layer[3].style.transform = 'translate(' + X/100*-6 + 'px, ' + Y/100*-6 + 'px)';
+// }
